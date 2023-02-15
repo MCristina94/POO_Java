a1560e4327cc63d4c58e86ea9e6ec21969c81517
Mcristina94 <cmaureen1908@gmail.com>
2023-02-15 14:00:32 -0500

    Primer clase 14 de feb de 2023, cononociendo lenguaje java

---
+++ Clase01_POO.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Perro
+        String nombrePerro = "Manchitas";
+        Integer edadPerro = 2;
+        String pesoPerro = "un kilo y medio";
+        String sonidoPerro = "wou wou";
+        System.out.println(nombrePerro + " come " + pesoPerro + " hace: " + sonidoPerro + " y tiene: " + edadPerro + "añitos" );
+
+        //Pez
+        String nombrePez = "Nemo";
+        Integer edadPez = 1;
+        String pesoPez = "140 gramos";
+        String sonidoPez = "glu glu";
+        System.out.println(nombrePez + " come " + pesoPez + " hace bajo el agua: " + sonidoPez + " y tiene: " + edadPez + "añitos" );
+
+        //Gato
+        String nombreGato = "Silvestre";
+        Integer edadGato = 3;
+        String pesoGato = "medio kilo";
+        String sonidoGato = "miau miau";
+        System.out.println(nombreGato + " come " + pesoGato + " hace: " + sonidoGato + " y tiene: " + edadGato + "añitos" );
+
+        //Tortuga
+        String nombreTortuga = "Manuelita";
+        Integer edadTortuga = 12;
+        String pesoTortuga = "300 gramos";
+        String vivienda = "Pehuajó";
+        System.out.println(nombreTortuga + " come: " + pesoTortuga + " vive en: " + vivienda + " y tiene: " + edadTortuga + "añitos" );
+
+        //Pajaro
+        String nombrePajaro = "Gardel";
+        Integer edadPajaro = 1;
+        String pesoPajaro = "350 gramos";
+        String onomatopeya = "Piola";
+        System.out.println(nombrePajaro + " come: " + pesoPajaro + " su sonido es un: " + onomatopeya + " y tiene: " + edadPajaro + "añito" );
+    }
+
+}
